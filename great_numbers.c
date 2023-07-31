c83b60b1a03ccbd7ef061cac15058a02c8092ae4
sindhur-ca21 <sindhur.ca21@kascsathy.ac.in>
2023-07-31 10:24:14 +0000

    first commit

---
+++ great_numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+  int a=30;
+  int b=20;
+
+   if(a>b) {
+     printf("a is greater than b \n");
+    }
+
+   else {
+     printf("b is less than a \n");
+     }
+}
